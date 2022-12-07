076b34c61834eaf9f7fd989b07703fa1041451d3
ASENNIU <13345752262qq.com>
2022-12-08 03:20:18 +0800

    使用逆向工程生成微服务后端基本代码

--- gulimall-ware/src/main/java/com/leon/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/leon/gulimall/ware/GulimallWareApplication.java
@@ -1,4 +1,4 @@
-package com.leon.gulimall.lware;
+package com.leon.gulimall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
