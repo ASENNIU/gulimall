285a246ce428e4a7316b1d0dd7fbba0c01d5ab75
ASENNIU <13345752262qq.com>
2022-12-09 02:56:11 +0800

    添加nacos和gateway

--- gulimall-member/src/main/java/com/leon/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/leon/gulimall/member/GulimallMemberApplication.java
@@ -2,7 +2,11 @@ package com.leon.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.leon.gulimall.member.feign")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
 
