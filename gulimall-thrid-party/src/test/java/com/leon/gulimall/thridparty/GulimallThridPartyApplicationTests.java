1d7a52d9bc6343ba0c3cacf4b79c4641d3f4d7b7
ASENNIU <1334575226@qq.com>
2022-12-19 23:42:42 +0800

    初始化第三方服务

---
+++ gulimall-thrid-party/src/test/java/com/leon/gulimall/thridparty/GulimallThridPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.gulimall.thridparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallThridPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
