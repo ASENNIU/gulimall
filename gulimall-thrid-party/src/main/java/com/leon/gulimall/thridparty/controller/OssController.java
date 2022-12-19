799423305a2c6b2a7d2c95aa6bd43bff5c0dc8dd
ASENNIU <1334575226@qq.com>
2022-12-20 01:33:43 +0800

    完成基于阿里OSS，利用后端签名完成文件上传

---
+++ gulimall-thrid-party/src/main/java/com/leon/gulimall/thridparty/controller/OssController.java
@@ -0,0 +1,83 @@
+package com.leon.gulimall.thridparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.leon.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+    @Autowired
+    OSS ossClient;
+    // 阿里云账号Acce
+    // ssKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+    @Value("${spring.cloud.alicloud.access-key}")
+    String accessId;
+    @Value("${spring.cloud.alicloud.secret-key}")
+    String accessKey;
+    // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+    @Value("${spring.cloud.alicloud.oss.endpoint}")
+    String endpoint;
+    // 填写Bucket名称，例如examplebucket。
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    String bucket;
+    // 填写Host地址，格式为https://bucketname.endpoint。
+
+
+    @RequestMapping("/oss/policy")
+    public R policy() {
+
+
+
+        //https://gulimall-hello.oss-cn-beijing.aliyuncs.com/hahaha.jpg
+
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+//        String callbackUrl = "http://88.88.88.88:8888";
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format;// 用户上传文件时指定的前缀。
+
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+
+        return R.ok().put("data",respMap);
+    }
+
+}
