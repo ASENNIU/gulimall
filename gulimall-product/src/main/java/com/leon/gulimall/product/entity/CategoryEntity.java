e407f23c1b7e3b4b624c84e11c6986ebc7cb0899
ASENNIU <1334575226@qq.com>
2022-12-21 23:08:41 +0800

    部分属性分组功能

--- gulimall-product/src/main/java/com/leon/gulimall/product/entity/CategoryEntity.java
+++ gulimall-product/src/main/java/com/leon/gulimall/product/entity/CategoryEntity.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
@@ -62,6 +63,7 @@ public class CategoryEntity implements Serializable {
 	 */
 	private Integer productCount;
 
+	@JsonInclude(JsonInclude.Include.NON_EMPTY) //不为空时才转化为json写出去
 	@TableField(exist = false)
 	private List<CategoryEntity> children;
 
--- gulimall-product/src/main/java/com/leon/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/leon/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -11,10 +11,35 @@ import com.leon.common.utils.Query;
 import com.leon.gulimall.product.dao.AttrGroupDao;
 import com.leon.gulimall.product.entity.AttrGroupEntity;
 import com.leon.gulimall.product.service.AttrGroupService;
+import org.springframework.util.StringUtils;
 
 
 @Service("attrGroupService")
 public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEntity> implements AttrGroupService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        if (catelogId == 0) {
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<AttrGroupEntity>()
+            );
+            return new PageUtils(page);
+        } else {
+            String key = (String) params.get("key");
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<AttrGroupEntity>().eq("catelog_id", key);
+            if(!StringUtils.isEmpty(key)) {
+                wrapper.and((obj) -> {
+                    obj.eq("attr_group_id", key).or().like("attr_group_name", key);
+                });
+            }
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    wrapper
+            );
+            return new PageUtils(page);
+        }
+    }
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
