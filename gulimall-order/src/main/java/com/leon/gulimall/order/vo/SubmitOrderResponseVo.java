9c3b759b4f89f24850d238d93327affdeef82d1d
ASENNIU <1334575226@qq.com>
2023-03-10 16:42:33 +0800

    完成订单模块，这个项目到这里就结束了

---
+++ gulimall-order/src/main/java/com/leon/gulimall/order/vo/SubmitOrderResponseVo.java
@@ -0,0 +1,15 @@
+package com.leon.gulimall.order.vo;
+
+import com.leon.gulimall.order.entity.OrderEntity;
+import lombok.Data;
+
+@Data
+public class SubmitOrderResponseVo {
+
+    private OrderEntity order;
+
+    /** 错误状态码 **/
+    private Integer code;
+
+
+}
